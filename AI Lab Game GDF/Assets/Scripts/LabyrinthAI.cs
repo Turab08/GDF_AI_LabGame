3abf66230863f4bc9cf931dfbf0b78b31bd8122b
Turab <turab.violin@gmail.com>
2026-02-21 13:15:14 +0400

    Finished - Simulation 2

--- AI Lab Game GDF/Assets/Scripts/LabyrinthAI.cs
+++ AI Lab Game GDF/Assets/Scripts/LabyrinthAI.cs
@@ -1,8 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class LabyrinthAI : MonoBehaviour
 {
+    [Header("NavMesh Agent")]
+    public NavMeshAgent agent;
+
+    public Transform endPoint;
+
+    [Header("Endpoint Postitions")]
+    public Transform[] endPointPositions;
+
+    void Start()
+    {
+        agent.SetDestination(endPoint.position);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeEndPoint();
+        }
+    }
     
+    void ChangeEndPoint()
+    {
+        int randomIndex = Random.Range(0, endPointPositions.Length);
+
+        endPoint.position = new Vector3(endPointPositions[randomIndex].position.x, endPoint.position.y, endPointPositions[randomIndex].position.z);
+        agent.SetDestination(endPoint.position);
+    }
 }
