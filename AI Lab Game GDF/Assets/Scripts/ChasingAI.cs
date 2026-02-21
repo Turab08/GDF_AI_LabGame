135794082a6f8727dd29eaffbaeff59f0ac729f0
Turab <turab.violin@gmail.com>
2026-02-21 14:40:56 +0400

    Fixed Some Bugs

--- AI Lab Game GDF/Assets/Scripts/ChasingAI.cs
+++ AI Lab Game GDF/Assets/Scripts/ChasingAI.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class ChasingAI : MonoBehaviour
 {
+    public NavMeshSurface surface;
     public NavMeshAgent agent;
     public Transform player;
+
+    void Awake()
+    {
+        surface.BuildNavMesh();
+    }
 
     void Update()
     {
--- AI Lab Game GDF/Assets/Scripts/LabyrinthAI.cs
+++ AI Lab Game GDF/Assets/Scripts/LabyrinthAI.cs
@@ -1,9 +1,12 @@
+using Unity.AI.Navigation;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class LabyrinthAI : MonoBehaviour
 {
+    public NavMeshSurface surface;
+
     [Header("NavMesh Agent")]
     public NavMeshAgent agent;
 
@@ -11,6 +14,12 @@ public class LabyrinthAI : MonoBehaviour
 
     [Header("Endpoint Postitions")]
     public Transform[] endPointPositions;
+
+    void Awake()
+    {
+        //Build the NavMesh when the game starts
+        surface.BuildNavMesh();
+    }
 
     void Start()
     {
@@ -27,8 +36,10 @@ public class LabyrinthAI : MonoBehaviour
     
     void ChangeEndPoint()
     {
+        //Pick a random index from the endPointPositions array
         int randomIndex = Random.Range(0, endPointPositions.Length);
 
+        //Set the endPoint's position to the position of the randomly selected endPointPosition
         endPoint.position = new Vector3(endPointPositions[randomIndex].position.x, endPoint.position.y, endPointPositions[randomIndex].position.z);
         agent.SetDestination(endPoint.position);
     }
